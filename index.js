ba6d79d697ebcacdb491d4acb42b2ada580b6837
Dpreshy <98194107+Dpreshy@users.noreply.github.com>
2022-03-18 11:37:31 +0100

    hosting

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv')
+
+const express = require ('express')
+const res = require('express/lib/response')
+const port = process.env.port || 2022
+const app = express()
+// const mongoose = require('mongoose')
+
+ app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+    res.send('Api is ready.....')
+})
+
+
+app.listen(port, ()=>{
+    console.log('listening to port' + port)
+})
